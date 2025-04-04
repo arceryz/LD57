fe84aacdd4947f5630d9ad673110b593ed1ef3f6
arceryz <t.j.m.vandervalk@proton.me>
2025-04-04 12:35:21 +0200

    Set up main menu with button and folder structure.

---
+++ scenes/main_menu/main_menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+@export var Level1: PackedScene
+@onready var PlayButton: Button = %PlayButton
+
+func _ready() -> void:
+	PlayButton.pressed.connect(_on_play_pressed)
+
+func _on_play_pressed():
+	get_tree().change_scene_to_packed(Level1)
