e5a934bae021b3c5f5d8381f132e3519f7441030
arceryz <t.j.m.vandervalk@proton.me>
2025-04-05 12:13:12 +0200

    Gameplay with flight.

---
+++ prefabs/player/player.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+const SPEED: float = 500.0
+const JUMP_VELOCITY: float = 700.0
+const JUMP_CHARGE_RESET_SEC: float = 2.0
+const FLY_VELOCITY: float = 700.0
+
+@onready var Wings: Node2D = $Wings
+@onready var Anim: AnimationPlayer = $Anim
+
+var jump_charges: int = 1
+
+func _physics_process(delta: float) -> void:
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = -JUMP_VELOCITY
+	else:
+		velocity += get_gravity() * delta
+		if Input.is_action_just_pressed("jump"):
+			flap()
+	
+	var direction := Input.get_axis("move_left", "move_right")
+	velocity.x = direction * SPEED if direction else move_toward(velocity.x, 0, SPEED)
+	move_and_slide()
+
+func is_falling() -> bool:
+	return velocity.y > 0 and not is_on_floor()
+
+func flap():
+	velocity.y = -FLY_VELOCITY
+	Anim.play("flap")
--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -5,6 +5,7 @@ extends Control
 
 func _ready() -> void:
 	PlayButton.pressed.connect(_on_play_pressed)
+	PlayButton.grab_focus()
 
 func _on_play_pressed():
 	get_tree().change_scene_to_packed(Level1)
